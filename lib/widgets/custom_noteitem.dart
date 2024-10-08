7ae3f9cf26638dfc99b75a1bf374e744bed94fc2
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-08 16:55:13 +0000

    create notes

---
+++ lib/widgets/custom_noteitem.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CustomNoteItem extends StatelessWidget {
+  const CustomNoteItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.orange,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16, bottom: 16),
+              child: Text(
+                'build your career with tharwat samy',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.5), fontSize: 18),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'May21 , 2022',
+              style: TextStyle(color: Colors.black.withOpacity(.5)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
