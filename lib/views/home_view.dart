a4fdb4473c2b00bd6c0f9c9a41f064f537e2cf27
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-09 18:20:16 +0000

    create home view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/Home_note_widget.dart';
+import 'package:note_app/widgets/add_noteitem.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -8,6 +9,19 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: NoteViewBody(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              context: context,
+              builder: (context) {
+                return AddNoteModalSheet();
+              });
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
