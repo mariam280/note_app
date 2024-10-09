e00e26f3d65ce9ed61b565728d77b0bd05f8a729
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-09 18:17:22 +0000

    create note item

---
+++ lib/widgets/add_noteitem.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_textfield.dart';
+
+class AddNoteModalSheet extends StatelessWidget {
+  const AddNoteModalSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 25,
+          ),
+          CustomTextField(
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            hint: 'content',
+            maxLines: 4,
+          ),
+        ],
+      ),
+    );
+  }
+}
