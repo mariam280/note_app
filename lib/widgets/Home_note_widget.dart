ee91db9bb01b1f70d8186149551915344582ba6e
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-09 18:20:34 +0000

    create home view widget

--- lib/widgets/Home_note_widget.dart
+++ lib/widgets/Home_note_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_appBar.dart';
 import 'package:note_app/widgets/custom_noteitem.dart';
+import 'package:note_app/widgets/note_listview.dart';
 
 class NoteViewBody extends StatelessWidget {
   const NoteViewBody({Key? key}) : super(key: key);
@@ -11,8 +12,14 @@ class NoteViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
+          SizedBox(
+            height: 25,
+          ),
           CustomAppBar(),
-          CustomNoteItem(),
+          SizedBox(
+            height: 25,
+          ),
+          Expanded(child: NoteListView()),
         ],
       ),
     );
