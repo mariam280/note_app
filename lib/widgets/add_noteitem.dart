8acfc914780f5c5821e25dd7dfa878d89129fd00
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-10 16:32:36 +0000

    create model sheet

--- lib/widgets/add_noteitem.dart
+++ lib/widgets/add_noteitem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_button.dart';
 import 'package:note_app/widgets/custom_textfield.dart';
 
 class AddNoteModalSheet extends StatelessWidget {
@@ -8,22 +9,28 @@ class AddNoteModalSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 25,
-          ),
-          CustomTextField(
-            hint: 'title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'content',
-            maxLines: 4,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 25,
+            ),
+            CustomTextField(
+              hint: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'content',
+              maxLines: 4,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
