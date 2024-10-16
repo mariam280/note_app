7bc594e3e4bb6fff2de7128cde5aa58454ce0246
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-16 19:52:24 +0000

    custom button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,16 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:note_app/views/edit_noteView.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key}) : super(key: key);
-
+  const CustomButton({Key? key, this.onTap}) : super(key: key);
+  final void Function()? onTap;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) {
-          return EditNoteView();
-        }));
-      },
+      onTap: onTap,
       child: Container(
         height: 55,
         width: MediaQuery.of(context).size.width,
