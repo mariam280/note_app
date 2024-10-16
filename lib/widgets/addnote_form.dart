dfb2a4b86668d3d28be7b0f6ceda09e074fa0b97
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-16 19:54:13 +0000

    create note form

---
+++ lib/widgets/addnote_form.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_button.dart';
+import 'package:note_app/widgets/custom_textfield.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({Key? key}) : super(key: key);
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  String? title, subtitle;
+  final GlobalKey<FormState> formKey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 25,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              title = value;
+            },
+            hint: 'title',
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(
+            onSaved: (value) {
+              subtitle = value;
+            },
+            hint: 'content',
+            maxLines: 4,
+          ),
+          SizedBox(
+            height: 32,
+          ),
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                return formKey.currentState!.save();
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
