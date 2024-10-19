a9be41802af8e60870efa3b63aedbf4087876f39
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-19 16:48:20 +0000

    note list view

--- lib/widgets/note_listview.dart
+++ lib/widgets/note_listview.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/read_note_cubit/read_note_cubit.dart';
+import 'package:note_app/models/note_model.dart';
 import 'package:note_app/widgets/custom_noteitem.dart';
 
 class NoteListView extends StatelessWidget {
@@ -6,14 +9,22 @@ class NoteListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 16),
-      child: ListView.builder(itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: CustomNoteItem(),
-        );
-      }),
-    );
+    return BlocBuilder<ReadNoteCubit, ReadNoteState>(builder: (context, state) {
+      List<NoteModel> notes =
+          BlocProvider.of<ReadNoteCubit>(context).notes ?? [];
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: CustomNoteItem(
+                  note: notes[index],
+                ),
+              );
+            }),
+      );
+    });
   }
 }
