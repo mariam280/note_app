4ca3b11a7f63824b87f1d65ac488bd4074f8951d
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-19 16:47:14 +0000

    custom icon

--- lib/widgets/custom_iconAppBar.dart
+++ lib/widgets/custom_iconAppBar.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 
 class IconAppBar extends StatelessWidget {
-  const IconAppBar({Key? key, required this.icon}) : super(key: key);
+  const IconAppBar({Key? key, required this.icon, this.onPressed})
+      : super(key: key);
   final IconData? icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 45,
-      width: 45,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.white.withOpacity(.1),
-      ),
-      child: Icon(icon),
-    );
+        height: 45,
+        width: 45,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.white.withOpacity(.1),
+        ),
+        child: IconButton(
+            onPressed: onPressed,
+            icon: Icon(
+              icon,
+              size: 27,
+            )));
   }
 }
