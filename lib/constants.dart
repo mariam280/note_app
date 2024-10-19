8d1a099d30fbcc26376d840b418b3f57b5954761
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-19 16:48:51 +0000

    constant file

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+List<Color> kColors = const [
+  Color(0xffD1BCE3),
+  Color(0xff227C9D),
+  Color(0xff17C3B2),
+  Color(0xffFFCB77),
+  Color(0xffF61067),
+];
