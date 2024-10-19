378bc2efde2cf636b9091833dd832553fd8f65fe
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-19 16:50:05 +0000

    colors list

---
+++ lib/widgets/colors_listview.dart
@@ -0,0 +1,64 @@
+colors listimport 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/constants.dart';
+import 'package:note_app/cubits/add_note_cubit/add_note_cubit.dart';
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({Key? key, required this.isActive, required this.color})
+      : super(key: key);
+  final bool isActive;
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? CircleAvatar(
+            radius: 38,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 35,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 38,
+          );
+  }
+}
+
+class ColorsListView extends StatefulWidget {
+  const ColorsListView({Key? key}) : super(key: key);
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: kColors.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: GestureDetector(
+                onTap: () {
+                  currentIndex = index;
+                  BlocProvider.of<NoteCubit>(context).color = kColors[index];
+                  setState(() {});
+                },
+                child: ColorItem(
+                  color: kColors[index],
+                  isActive: currentIndex == index,
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
