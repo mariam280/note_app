9224629b7a58996d79c53ccb1b09cb9c918ad105
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-19 16:46:41 +0000

    custom appBar

--- lib/widgets/custom_appBar.dart
+++ lib/widgets/custom_appBar.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_iconAppBar.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({Key? key, required this.title, required this.icon})
+  const CustomAppBar(
+      {Key? key, required this.title, required this.icon, this.onPressed})
       : super(key: key);
   final String title;
   final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -18,6 +20,7 @@ class CustomAppBar extends StatelessWidget {
           flex: 1,
         ),
         IconAppBar(
+          onPressed: onPressed,
           icon: icon,
         ),
       ],
