34ec9b6d14b6582aecfdf719b7676ca1cbcb6bf6
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-19 16:45:42 +0000

    note widget

--- lib/widgets/Home_note_widget.dart
+++ lib/widgets/Home_note_widget.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/cubits/read_note_cubit/read_note_cubit.dart';
 import 'package:note_app/widgets/custom_appBar.dart';
 import 'package:note_app/widgets/custom_noteitem.dart';
 import 'package:note_app/widgets/note_listview.dart';
 
-class NoteViewBody extends StatelessWidget {
+class NoteViewBody extends StatefulWidget {
   const NoteViewBody({Key? key}) : super(key: key);
+
+  @override
+  State<NoteViewBody> createState() => _NoteViewBodyState();
+}
+
+class _NoteViewBodyState extends State<NoteViewBody> {
+  @override
+  void initState() {
+    BlocProvider.of<ReadNoteCubit>(context).fetchAllNotes();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
