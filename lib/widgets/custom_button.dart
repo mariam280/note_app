710d815608c406e4591cd1de2d328694a3038798
Neama_Mohammed <neamamohammed649@gmail.com>
2024-10-19 16:46:59 +0000

    custom Button

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:note_app/views/edit_noteView.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({Key? key, this.onTap}) : super(key: key);
+  const CustomButton({Key? key, this.onTap, this.isLoading = false})
+      : super(key: key);
   final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -16,14 +18,22 @@ class CustomButton extends StatelessWidget {
           color: Colors.blue,
         ),
         child: Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          child: isLoading
+              ? SizedBox(
+                  height: 24,
+                  width: 24,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : Text(
+                  'Add',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
         ),
       ),
     );
